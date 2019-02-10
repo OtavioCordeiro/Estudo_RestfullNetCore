24ec4be836a85f80b8b56cccbbf99d0d298a2796
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-02-10 09:43:23 -0200

    ConfiguraMapper

--- Library.API.Tests/Controllers/AuthorsControllerTests.cs
+++ Library.API.Tests/Controllers/AuthorsControllerTests.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using FluentAssertions;
 using Library.API.Controllers;
 using Library.API.Entities;
+using Library.API.Extensions;
 using Library.API.Models;
 using Library.API.Services.Interfaces;
 using Library.API.Tests.AutoData;
@@ -51,16 +52,25 @@ namespace Library.API.Tests.Controllers
         {
             try
             {
-                var ok = Mapper.Instance;
+                Mapper.AssertConfigurationIsValid();
             }
             catch (InvalidOperationException)
             {
-                Mapper.Initialize(
-                cfg =>
-                {
-                    cfg.CreateMap<Author, AuthorDto>();
-                });
+                InitializeMapper();
             }
+        }
+
+        private static void InitializeMapper()
+        {
+            Mapper.Initialize(
+                            cfg =>
+                            {
+                                cfg.CreateMap<Author, AuthorDto>()
+                                    .ForMember(dest => dest.Name,
+                                                opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                                    .ForMember(dest => dest.Age,
+                                                opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+                            });
         }
     }
 }
