474b773dcb856642a28b5ce62c8e8dcb61b53d43
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-02-16 19:24:35 -0200

    Comenta temporariamente até descobrir como fazer esses requests via xml

--- Library.API/Models/AuthorForCreationDto.cs
+++ Library.API/Models/AuthorForCreationDto.cs
@@ -10,7 +10,7 @@ namespace Library.API.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Genre { get; set; }
-        public DateTimeOffset DateOfBirth { get; set; }
-        public ICollection<BookForCreationDto> Books { get; set; }
+        ////public DateTimeOffset DateOfBirth { get; set; }
+        ////public ICollection<BookForCreationDto> Books { get; set; }
     }
 }
