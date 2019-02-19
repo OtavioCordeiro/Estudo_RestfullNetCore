c936c7f9b8a7ebf4b5fac3862ec962d1cc913a7e
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-02-18 23:30:34 -0300

    Resolve DatetimeOffset para xml

--- Library.API/Models/AuthorForCreationDto.cs
+++ Library.API/Models/AuthorForCreationDto.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Library.API.Models
 {
@@ -10,7 +11,17 @@ namespace Library.API.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Genre { get; set; }
-        ////public DateTimeOffset DateOfBirth { get; set; }
-        ////public ICollection<BookForCreationDto> Books { get; set; }
+
+        [XmlElement("DateOfBirth")]
+        public string DateOfBirthForXml
+        {
+            get { return DateOfBirth.ToString("o"); }
+            set { DateOfBirth = DateTimeOffset.Parse(value); }
+        }
+
+        [XmlIgnore]
+        public DateTimeOffset DateOfBirth { get; set; }
+        [XmlIgnore]
+        public ICollection<BookForCreationDto> Books { get; set; }
     }
 }
