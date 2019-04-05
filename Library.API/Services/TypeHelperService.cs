c7f89e675dbd14d5ace54955df03b62968748014
Otavio Cordeiro Lopes <olopes@braspag.com.br>
2019-04-05 16:59:35 -0300

    remove usings não utilizavel

--- Library.API/Services/TypeHelperService.cs
+++ Library.API/Services/TypeHelperService.cs
@@ -1,9 +1,5 @@
 ﻿using Library.API.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Library.API.Services
 {
